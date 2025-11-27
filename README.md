daba90b9014e69ec0b55bab2b535510721a54377
shabaz7323 <shabaz.mahesh@gmail.com>
2025-11-27 10:53:44 +0530

    push

---
+++ README.md
@@ -0,0 +1,2 @@
+# GitOps with ArgoCD
+This repo contains Node.js + Redis microservices deployed via ArgoCD.
---
+++ services/api/server.js
@@ -0,0 +1,1 @@
+const express=require('express');const app=express();app.get('/health',(req,res)=>res.send('ok'));app.get('/items',(req,res)=>res.json([{id:1}]));app.listen(3000);
